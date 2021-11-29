3926d4424d4bf2b787365a7a66095648e862fa23
guodingfang <guodingfang1@163.com>
2021-11-29 19:33:18 +0800

    dev

---
+++ components/article/comment-item/index.js
@@ -0,0 +1,34 @@
+// components/article/comment-item/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: ''
+    },
+    userInfo: {
+      type: Object,
+      value: null
+    },
+    isShowAll: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
